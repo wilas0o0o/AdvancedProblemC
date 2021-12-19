63ae26d3d8f99375c7a9ba0d9877dbfcf5e905b9
wilas0o0o <03mcu30@gmail.com>
2021-12-19 11:17:30 +0000

    [Start]応用課題7c

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   attachment :profile_image, destroy: false
   has_many :favorites,dependent: :destroy
   has_many :book_comments,dependent: :destroy
+  has_many :group_users,dependent: :destroy
 
   has_many :active_relationships,class_name: "Relationship",foreign_key: "follower_id",dependent: :destroy
   has_many :passive_relationships,class_name: "Relationship",foreign_key: "followed_id",dependent: :destroy
-
   has_many :followings,through: :active_relationships,source: :followed
   has_many :followers,through: :passive_relationships,source: :follower
 
